9a4de8340842efce75ce53983cf377f01121db54
Luis Abreu <luisandre_69@hotmail.com>
2022-08-11 23:37:21 +0100

    base

---
+++ cypress/integration/example.js
@@ -0,0 +1,9 @@
+describe('Cypress Tests with Docker', () => {
+	it('should load website', () => {
+		cy.visit('https://www.example.com')
+	})
+
+	it('should load h1 element', () => {
+		cy.get('h1').should('be.visible')
+	})
+})
